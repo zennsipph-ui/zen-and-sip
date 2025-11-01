0677c731fddba19ee06d1aa9eaa605538495e5ef
zennsipph-ui <zennsip.ph@gmail.com>
2025-11-01 22:23:28 +0900

    Add files via upload

---
+++ zen-and-sip-starter-shop/zen-and-sip-starter-shop/config.js
@@ -0,0 +1,5 @@
+window.SHOP_CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbxEAkQHM17G3aZcWnXrMwssUisJFCUfLUyMNQbcYhE0mqTIm5v7CmoTQ74JlkkHUDkc/exec",
+  SHARED_SECRET: "zenNsipwillbebig1121",
+  IG_URL: "https://www.instagram.com/zennsip.ph"
+};
